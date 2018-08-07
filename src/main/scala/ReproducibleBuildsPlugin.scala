5a6b796812f37d350ba1045fcfd35f8ed996d0b3
Arnout Engelen <arnouten@bzzt.net>
2018-08-07 20:15:42 +0200

    Preliminary support for generating .buildinfo files

--- src/main/scala/ReproducibleBuildsPlugin.scala
+++ src/main/scala/ReproducibleBuildsPlugin.scala
@@ -1,14 +1,21 @@
 package net.bzzt.reproduciblebuilds
 
-import sbt.{ AutoPlugin, Compile, Plugins, File}
-import sbt.Keys._
-import sbt.plugins.JvmPlugin
+import java.nio.charset.Charset
+import java.nio.file.{Files, Paths}
+import java.security.MessageDigest
 
+import sbt.Defaults._
+import sbt.{Artifact, AutoPlugin, Compile, File, Plugins, ScalaVersion, taskKey}
+import sbt.Keys.{scalaVersion, _}
+import sbt.plugins.JvmPlugin
 import io.github.zlika.reproducible._
 
 object ReproducibleBuildsPlugin extends AutoPlugin {
   // To make sure we're loaded after the defaults
   override def requires: Plugins = JvmPlugin
+
+  val reproducibleBuildsCertification = taskKey[File]("Create a Reproducible Builds certification")
+  val reproducibleBuildsUploadCertification = taskKey[Unit]("Upload the Reproducible Builds certification")
 
   override lazy val projectSettings = Seq(
     packageBin in Compile := {
@@ -19,6 +26,68 @@ object ReproducibleBuildsPlugin extends AutoPlugin {
         .addFileStripper("META-INF/maven/\\S*/pom.properties", new PomPropertiesStripper())
         .strip(bin, out)
       out
+    },
+    artifactPath in reproducibleBuildsCertification := artifactPathSetting(artifact in reproducibleBuildsCertification).value,
+    reproducibleBuildsCertification := {
+      val groupId = organization.value
+      val packagedFile = (packageBin in Compile).value
+      val packageName = moduleName.value
+
+      val targetDirPath = crossTarget.value
+      val packageVersion = version.value
+      val targetFilePath = targetDirPath.toPath.resolve(targetFilename(packageName, packageVersion, "TODO"))
+
+      val bytes = Files.readAllBytes(packagedFile.toPath)
+
+      val digest = MessageDigest.getInstance("SHA-256")
+      val checksum = digest.digest(bytes).map("%02x" format _).mkString
+
+      val content = Map(
+        "Format" -> "1.8",
+        "Build-Architecture" -> "all",
+        "Source" -> packageName,
+        "Binary" -> packageName,
+        "Package" -> packageName,
+        // Strictly spoken not allowed by https://wiki.debian.org/ReproducibleBuilds/BuildinfoFiles#Field_descriptions,
+        // but jars should typically be architecture-independent...
+        "Architecture" -> "all",
+        "Version" -> packageVersion,
+        "Checksums-Sha256" -> s"\n $checksum ${bytes.length} ${packagedFile.getName}",
+        // Extra 'custom' fields:
+        "ScalaVersion" -> (scalaVersion in artifactName).value,
+        "ScalaBinaryVersion" -> (scalaBinaryVersion in artifactName).value,
+      )
+
+      import collection.JavaConverters._
+      Files.write(targetFilePath, formatControlFile(content).asJava, Charset.forName("UTF-8"))
+
+      targetFilePath.toFile
+    },
+    reproducibleBuildsUploadCertification := {
+      println(reproducibleBuildsCertification.value)
     }
   )
+
+  /**
+    * https://www.debian.org/doc/debian-policy/ch-controlfields.html#syntax-of-control-files
+    *
+    * @param content key->value pairs
+    * @return formatted lines
+    */
+  private def formatControlFile(content: Map[String, String]): Iterable[String] =
+    // Dummy implementation ;)
+    content.map { case (key, value) => s"$key: $value" }
+
+  /**
+    * Determine the target filename.
+    *
+    * See https://wiki.debian.org/ReproducibleBuilds/BuildinfoFiles#File_name_and_encoding
+    *
+    * @param source
+    * @param version
+    * @param suffix
+    * @return
+    */
+  def targetFilename(source: String, version: String, suffix: String) =
+    source + "_" + version + "_" + suffix + ".buildinfo"
 }

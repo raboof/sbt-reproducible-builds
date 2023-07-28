9ad3e83e3603a5070e6ac50f496b20187029a7d7
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-07-28 10:56:20 +0200

    Apply scalafmt

--- src/main/scala/SbtNativePackagerHelpers.scala
+++ src/main/scala/SbtNativePackagerHelpers.scala
@@ -5,19 +5,15 @@ import com.typesafe.sbt.packager.universal.UniversalPlugin.autoImport._
 import sbt._
 import sbt.Keys._
 
-/**
-  * Helper code for sbt-native-packager integration.
+/** Helper code for sbt-native-packager integration.
   *
-  * The main ReproducibleBuildsPlugin code should not rely on any
-  * sbt-native-packager code, since this plugin should be usable without
-  * sbt-native-packager and not introduce it to projects not already using it.
+  * The main ReproducibleBuildsPlugin code should not rely on any sbt-native-packager code, since this plugin should be
+  * usable without sbt-native-packager and not introduce it to projects not already using it.
   *
-  * Any code dependent on sbt-native-packager classes
-  * should go here, and only be called in case it is indeed available.
+  * Any code dependent on sbt-native-packager classes should go here, and only be called in case it is indeed available.
   *
-  * To avoid 'leaking' references to sbt-native-packager classes into other
-  * classes, care should be taken that none appear in parameter or return
-  * value types either.
+  * To avoid 'leaking' references to sbt-native-packager classes into other classes, care should be taken that none
+  * appear in parameter or return value types either.
   */
 object SbtNativePackagerHelpers {
   val plugin: Plugins.Basic = UniversalPlugin
@@ -30,7 +26,7 @@ object SbtNativePackagerHelpers {
         case None =>
           throw new IllegalStateException(
             "Reference to undefined setting: packageBin in Universal. " +
-            "Have you enabled a sbt-native-packager plugin?"
+              "Have you enabled a sbt-native-packager plugin?"
           )
       }
     }
